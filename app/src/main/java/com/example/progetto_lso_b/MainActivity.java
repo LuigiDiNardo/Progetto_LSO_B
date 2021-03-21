97492bdc13dff740fb32e1152fee48b1347a3885
LuigiDN <gigidinardo2000@gmail.com>
2021-03-21 01:01:12 +0100

    Sono stati inseriti i fragment nei container; inoltre sono stati aggiunti i vari layout dei fragment. In futuro verranno riadattati in base alle necessità del codice che verrà trascritto in futuro.

--- app/src/main/java/com/example/progetto_lso_b/MainActivity.java
+++ app/src/main/java/com/example/progetto_lso_b/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.progetto_lso_b;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.provider.Contacts;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BottomNavigationView buttonNV= findViewById(R.id.menu);
+        buttonNV.setOnNavigationItemSelectedListener(navListener);
+
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.online_frame,new OnlineStatusFragment()).commit();
+        getSupportFragmentManager().beginTransaction().replace(R.id.people_frame,new PeopleFragment()).commit();
+        getSupportFragmentManager().beginTransaction().replace(R.id.messages_frame,new MessagesFragment()).commit();
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedItem = null;
+                    switch(item.getItemId()){
+
+                        case R.id.Home:
+                            break;
+                        case R.id.Message:
+                            break;
+                        case R.id.Search:
+                            break;
+                        case R.id.Setting:
+                            break;
+                        default:
+                            break;
+                    }
+
+                    return true;
+                }
+            };
+
+
+
 }
---
+++ app/src/main/java/com/example/progetto_lso_b/PeopleFragment.java
@@ -0,0 +1,25 @@
+package com.example.progetto_lso_b;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class PeopleFragment extends Fragment {
+
+    public PeopleFragment() {}
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.people_fragment, container, false);
+    }
+}
