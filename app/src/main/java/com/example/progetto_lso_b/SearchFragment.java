60747dd46d49093420c9e3703d761c211ac77d11
LuigiDN <gigidinardo2000@gmail.com>
2021-03-23 20:28:35 +0100

    Aggiunto un Navigation Component per gestire la navigazione tra i vari settori dell'app; dunque inseriti il Grafico di navigazione del component e la relazione Rispetto al classico passaggio tra frame tramite id è stato utilizzato il metodo navigate(<destination>) il quale, questo, si basa solo sull'id destinazione e non dell'id azione stabilito nel file nav_graph.xml . Inoltre, è stato aggiunto un controllo che per ogni primo avvio dell'app mostrerà sempre la home (almeno questo avverrà fino alla creazione del login).

---
+++ app/src/main/java/com/example/progetto_lso_b/SearchFragment.java
@@ -0,0 +1,21 @@
+package com.example.progetto_lso_b;
+
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SearchFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_search, container, false);
+    }
+}
