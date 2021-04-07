512b90429f0d879337a36fdbcab98edec0e0a484
LuigiDiNardo <luigidinardo.work@gmail.com>
2021-04-07 10:58:34 +0200

    Implementata la mappa che mostra la posizione dell'utente in quell'istante, con tanto di marker che identifica visivamente la locazione. Ciò è stato implementato grazie ad osmdroid, un set di API che consentono di utilizzare strumenti per la visualizzazione di mappe su android. Sono state aggiunte anche le eventuali richieste d'autorizzazioni per poter gestire la suddetta mappa e la posizione.

--- app/src/main/java/com/example/progetto_lso_b/MainActivity.java
+++ app/src/main/java/com/example/progetto_lso_b/MainActivity.java
@@ -3,12 +3,15 @@ package com.example.progetto_lso_b;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.app.NavUtils;
 import androidx.fragment.app.FragmentManager;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.MenuItem;
 
@@ -19,14 +22,20 @@ public class MainActivity extends AppCompatActivity {
 
     static boolean first_log=true; //variabile per indicare se la home è caricata la prima volta o meno
     NavController navController;
+    private static final String[] permessi = new String[]{
+            Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+    private final static int requestcode = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this,permessi,requestcode);
+        }
         if (first_log){ //se è la prima volta che si va nella home si carica automaticamente il fragment; altrimenti lo gestisce il navController (guarda sotto)
             getSupportFragmentManager().beginTransaction().replace(R.id.online_frame, new OnlineStatusFragment()).commit();
             getSupportFragmentManager().beginTransaction().replace(R.id.people_frame, new PeopleFragment()).commit();
@@ -71,6 +80,13 @@ public class MainActivity extends AppCompatActivity {
                 }
             };
 
+
+    public static String[] getPermessi() {
+        return permessi;
+    }
+    public static int getRequestcode() {
+        return requestcode;
+    }
 
     //@Override
    // public boolean onOptionsItemSelected(@NonNull MenuItem item) {
